e4d1eec4ed320df37da99e0217e5660858881b3e
Christopher Gammie <contact@gammie.co.uk>
2021-04-23 11:20:07 +0100

    [Feature] Add initial implementation

---
+++ src/Drivers/SoftDeleteBooleanDriver.php
@@ -0,0 +1,136 @@
+<?php
+/*
+ * Copyright 2021 Cloud Creativity Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace LaravelJsonApi\BooleanSoftDeletes\Drivers;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use InvalidArgumentException;
+use LaravelJsonApi\Eloquent\Drivers\StandardDriver;
+use Webkid\LaravelBooleanSoftdeletes\SoftDeletesBoolean;
+use function boolval;
+
+class SoftDeleteBooleanDriver extends StandardDriver
+{
+
+    /**
+     * SoftDeleteDriver constructor.
+     *
+     * @param Model|SoftDeletesBoolean $model
+     */
+    public function __construct($model)
+    {
+        if (!in_array(SoftDeletesBoolean::class, class_uses($model), true)) {
+            throw new InvalidArgumentException('Expecting a model that is boolean soft-deletable.');
+        }
+
+        parent::__construct($model);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function query(): Builder
+    {
+        /**
+         * When querying specific resources, we use `withTrashed` as we want trashed
+         * resources to exist in our API.
+         */
+        return parent::query()->withTrashed();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function persist(Model $model): bool
+    {
+        /**
+         * If the model is being restored, the restore method executes a
+         * save on the model. So we only need to run the restore method and all
+         * dirty attributes will be saved.
+         */
+        if ($this->willRestore($model)) {
+            return $this->restore($model);
+        }
+
+        /**
+         * To ensure Laravel still executes its soft-delete logic (e.g. firing events)
+         * we need to delete before a save when we are soft-deleting. Although this
+         * may result in two database calls in this scenario, it means we can guarantee
+         * that standard Laravel soft-delete logic is executed.
+         */
+        if ($this->willSoftDelete($model)) {
+            $model->delete();
+        }
+
+        return (bool) $model->save();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function destroy(Model $model): bool
+    {
+        return (bool) $model->forceDelete();
+    }
+
+    /**
+     * @param Model|SoftDeletesBoolean $model
+     * @return bool
+     */
+    private function restore(Model $model): bool
+    {
+        return (bool) $model->restore();
+    }
+
+    /**
+     * Will the hydration operation restore the model?
+     *
+     * @param Model|SoftDeletesBoolean $model
+     * @return bool
+     */
+    private function willRestore(Model $model): bool
+    {
+        if (!$model->exists) {
+            return false;
+        }
+
+        $column = $model->getIsDeletedColumn();
+
+        return false !== boolval($model->getOriginal($column)) && false === boolval($model->{$column});
+    }
+
+    /**
+     * Will the hydration operation result in the model being soft deleted?
+     *
+     * @param Model|SoftDeletesBoolean $model
+     * @return bool
+     */
+    private function willSoftDelete(Model $model): bool
+    {
+        if (!$model->exists) {
+            return false;
+        }
+
+        $column = $model->getIsDeletedColumn();
+
+        return false === boolval($model->getOriginal($column)) && false !== boolval($model->{$column});
+    }
+
+}
---
+++ tests/app/Schemas/PostSchema.php
@@ -0,0 +1,88 @@
+<?php
+/*
+ * Copyright 2021 Cloud Creativity Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace App\Schemas;
+
+use App\Models\Post;
+use LaravelJsonApi\BooleanSoftDeletes\SoftDeletesBoolean;
+use LaravelJsonApi\Contracts\Pagination\Paginator;
+use LaravelJsonApi\Eloquent\Fields\Boolean;
+use LaravelJsonApi\Eloquent\Fields\DateTime;
+use LaravelJsonApi\Eloquent\Fields\ID;
+use LaravelJsonApi\Eloquent\Fields\Str;
+use LaravelJsonApi\Eloquent\Filters\OnlyTrashed;
+use LaravelJsonApi\Eloquent\Filters\WhereIdIn;
+use LaravelJsonApi\Eloquent\Filters\WithTrashed;
+use LaravelJsonApi\Eloquent\Pagination\PagePagination;
+use LaravelJsonApi\Eloquent\Schema;
+
+class PostSchema extends Schema
+{
+
+    use SoftDeletesBoolean;
+
+    /**
+     * The model the schema corresponds to.
+     *
+     * @var string
+     */
+    public static string $model = Post::class;
+
+    /**
+     * @var array|null
+     */
+    protected ?array $defaultPagination = ['number' => '1'];
+
+    /**
+     * @inheritDoc
+     */
+    public function fields(): iterable
+    {
+        return [
+            ID::make(),
+            DateTime::make('createdAt')->sortable()->readOnly(),
+            Str::make('content'),
+            Boolean::make('isDeleted')->unguarded(),
+            Str::make('slug')->sortable(),
+            Str::make('title'),
+            DateTime::make('updatedAt')->sortable()->readOnly(),
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function filters(): iterable
+    {
+        return [
+            WhereIdIn::make($this),
+            OnlyTrashed::make('trashed'),
+            WithTrashed::make('withTrashed'),
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function pagination(): ?Paginator
+    {
+        return PagePagination::make();
+    }
+}
+
